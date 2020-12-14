049e5a752448b3b7f323c0cc202caa062ede0373
Amol Kulkarni <16808069+Amol-Kulkarni91@users.noreply.github.com>
2020-12-14 15:34:05 -0500

    Update Web_Scraping_File.R

--- Web_Scraping_File.R
+++ Web_Scraping_File.R
@@ -1,74 +1,29 @@
+# load the necessary libraries
+
 library(rvest)
 library(dplyr)
+library("writexl")
+
+# Parse the contents using the read_html function
 
 plastic_ind <- read_html("https://www.iqsdirectory.com/plastics/plastics-2/")
 
-
-
+# Extract the html node matching the css selector object and convert it into text using html_text
 
 name <- plastic_ind %>% html_nodes("a span") %>% 
   html_text()
-
-
 city_state <- plastic_ind %>% html_nodes(".addr span") %>% 
   html_text()
-
 tel <- plastic_ind %>% html_nodes(".addr a") %>% 
   html_text()
 
+# Combine the text files into a dataframe
 
 plastic_industry <- data.frame(Name = name, City = city_state, phone = tel)
 
+# Separate the City and State from the address into their features
 
 plastic_industry <- plastic_industry %>% separate(City, c("City", "State"), ",")
 
-
-library("writexl")
-write_xlsx(plastic_industry,"C:\\Users\\amolk\\Box Sync\\Facepiece Respirator Project\\plastic_industries.xlsx")
-
-
-
-
-url <- "https://www.thomasnet.com/local.html?cov=NA&heading=60109006&radius=1000&sortby=Featured&what=plastics&which=prod&zip=16801&pg=1"
-
-url %>% read_html() %>% html_nodes(".profile-card__location+ span , .profile-card__location a , .profile-card__title a") %>% html_text()
-
-ls <- paste("https://www.thomasnet.com/local.html?cov=NA&heading=60109006&radius=1000&sortby=Featured&what=plastics&which=prod&zip=16801&pg=", 1:21, sep ="")
-
-names <- {}
-locations <- {}
-types <- {}
-for (i in ls){
-   name <- i %>% read_html() %>%
-              html_nodes(".profile-card__title a") %>%
-                  html_text()
-   location <- i %>% read_html() %>%
-                  html_nodes(".profile-card__location a") %>% html_attr("href")
-   
-   type <- i %>% read_html() %>%
-              html_nodes(".profile-card__location+ span") %>%
-                html_text()
-   
-   names <- c(names, name)
-   locations <- c(locations, location)
-   types <- c(types, type)
-}
-
-address <- {}
-types1 <- {}
-for (j in locations){
-  url <- paste("https://www.thomasnet.com", j, sep = "")
-  #add <- url %>% read_html() %>%
-  #              html_nodes(".addrline") %>% html_text()
-  
-  type1 <- url %>% read_html() %>%
-                html_node(".supplier-badge--mobile-icon+ span") %>% html_text()
-  
-  #address <- c(address, add)        
-  types1 <- c(types1, type1)
-  
-}
-
-
-plastic_ind <- data.frame(Name = names, Type = types, Location = address)
-write_xlsx(plastic_ind,"C:\\Users\\amolk\\Box Sync\\Facepiece Respirator Project\\plastic_industries_thomas.xlsx")
+# Write into an excel file
+write_xlsx(plastic_industry,"filepath//plastic_industries.xlsx")
